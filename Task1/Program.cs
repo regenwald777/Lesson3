7a37e8ebab97c9389cf23ff857377ee4039bf352
goliathus <goliathus@mail.ru>
2024-03-17 11:58:43 +0500

    add lesson2:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 4, 3);
+Console.WriteLine(result);
